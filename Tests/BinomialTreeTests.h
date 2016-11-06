948090986e9e118d8fe7257249c6d8285e9a7e47
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2016-11-06 17:05:41 +0100

    Fixed memory leaks from Binomial Tree

--- Tests/BinomialTreeTests.h
+++ Tests/BinomialTreeTests.h
@@ -13,13 +13,40 @@ private:
         return f - s;
     }
 
-    //TODO do tests
+    static void FourNumbers()
+    {
+        BinomialTree<int,comp> tree1(5,3,9,1);
+        assert(tree1.Top() == 1);
+        auto inner1 = tree1.InnerTrees();
+        assert(inner1.Size()==2);
+        auto mov = inner1.Begin();
+        auto left = *mov.GetValue();
+        mov.Next();
+        auto right = *mov.GetValue();
+        assert(right.Top() == 9);
+        assert(left.Top() == 3);
+        auto rest = left.InnerTrees();
+        assert(rest.Size() == 1);
+        assert(rest.Begin().GetValue()->Top() == 5);
+    }
+
+    static void TwoNumbers()
+    {
+        BinomialTree<int,comp> tree1(5,3);
+        assert(tree1.Top() == 3);
+        auto inner1 = tree1.InnerTrees();
+        assert(inner1.Size()==1);
+        assert(inner1.Begin().GetValue()->Top() == 5);
+    }
 
 public:
     static void tests()
     {
-        BinomialTree<int,comp> tree(5,3,9,1,-8,10,4,21);
-        BinomialTree<int,comp> second = tree;
+        TwoNumbers();
+
+        FourNumbers();
+        //BinomialTree<int,comp> tree(5,3,9,1,-8,10,4,21);
+        //BinomialTree<int,comp> second = tree;
     }
 };
 
