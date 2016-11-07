c6a4b804110b16288082217b0bfcfd1a2185dcce
Patrik Valkovic <patrik.valkovic@hotmail.cz>
2016-11-07 11:45:43 +0100

    Implemented valid BinomialHeapTests

--- precompile.sh
+++ precompile.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 g++ -E -D _STALIB_ Templates.h | grep . | grep -v "^#" > temp.h
+touch templates.h
 rm templates.h
 touch templates.h
 echo "#include <cstring>" >> templates.h
 echo "#include <iostream>" >> templates.h
 echo "#include <exception>" >> templates.h
+cat temp.h >> templates.h
+rm temp.h
